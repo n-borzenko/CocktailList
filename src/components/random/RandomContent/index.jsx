3bb39791b00f3892a03f53ff27be185ec5ab6c40
Natalia Borzenko <n-borzenko93@yandex.ru>
2018-11-16 21:58:40 +0300

    basic random animation

--- src/components/random/RandomContent/index.jsx
+++ src/components/random/RandomContent/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { createRandomTitle } from "../../../helpers/title";
+import BackgroundImage from "../../common/BackgroundImage";
 
 import "./RandomContent.css";
 
@@ -10,7 +11,72 @@ class RandomContent extends Component {
     }
 
     render() {
-        return <div>RandomContent</div>;
+        return (
+            <div className="random-content">
+                <div className="random-content__cards">
+                    <div className="random-content__card">
+                        <div className="random-content__background" />
+                        <div className="random-content__content">
+                            dfvjksdsdf
+                            <br />
+                            vjdfksbjvlsdf
+                            <br />
+                            sdfjvndlkf
+                            <br />
+                            12342
+                            <br />
+                            vjdfksbjvlsdf
+                            <br />
+                            sdfjvndlkf
+                            <br />
+                            12342
+                            <br />
+                            vjdfksbjvlsdf
+                            <br />
+                            sdfjvndlkf
+                            <br />
+                            12342
+                            <br />
+                            vjdfksbjvlsdf
+                            <br />
+                            sdfjvndlkf
+                            <br />
+                            12342
+                        </div>
+                    </div>
+                    <div className="random-content__card random-content__card_animated">
+                        <div className="random-content__background" />
+                        <div className="random-content__content">
+                            qvwervewrv
+                            <br />
+                            jdcghuhasdvjher
+                            <br />
+                            jkshdbkjashvbkjqvwervewrv
+                            <br />
+                            jdcghuhasdvjher
+                            <br />
+                            jkshdbkjashvbkj
+                            <br />
+                            jdcghuhasdvjher
+                            <br />
+                            jkshdbkjashvbkjqvwervewrv
+                            <br />
+                            jdcghuhasdvjher
+                            <br />
+                            jkshdbkjashvbkj
+                            <br />
+                            jdcghuhasdvjher
+                            <br />
+                            jkshdbkjashvbkjqvwervewrv
+                            <br />
+                            jdcghuhasdvjher
+                            <br />
+                            jkshdbkjashvbkj
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
